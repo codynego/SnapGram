0c65010b66de8b2a0500c200cc850af6f1a0e031
Abednego Emonena <emonenaabednego@gmail.com>
2023-11-04 13:43:55 +0100

    make a few chanes

---
+++ Guide.md
@@ -0,0 +1,14 @@
+# How this project was setup
+
+## Step 1
+ - create an account with appwrite
+ - create a new project
+
+## step 2
+### initialize a new react project using vite
+    - npm create vite@latest
+    `
+    `follow the prompt
+    `select react,then typescript
+    `
+
--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,29 @@
-const app = () => {
+import './globals.css';
+import { Routes, Route } from 'react-router-dom';
+import SigninForm from './_auth/forms/SigninForm';
+import SignUpForm from './_auth/forms/SignUpForm';
+import { Home } from './_root/pages';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+
+const App = () => {
   return (
-    <div>app</div>
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path='/sign-up' element={<SignUpForm />} />
+          <Route path="/sign-in" element={<SigninForm />} />
+        </Route>
+
+
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
   )
 }
 
-export default app
+export default App;
